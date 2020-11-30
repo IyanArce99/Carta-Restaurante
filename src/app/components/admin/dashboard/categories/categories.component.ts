e3703b9d96ba246d2cbcbcdf34a41b60506fb6d1
iyan <iyan.arcega@neozink.com>
2020-11-30 22:15:03 +0100

    CRUD Categorias Completo

--- src/app/components/admin/dashboard/categories/categories.component.ts
+++ src/app/components/admin/dashboard/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../../../services/category.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-categories',
@@ -8,17 +9,38 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-  loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
+  categoryForm = new FormGroup({
+    name: new FormControl('')
   });
+  categoryList: Category[];
 
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryService.getCategories();
+    this.categoryService.getCategories().snapshotChanges().subscribe( category => {
+      this.categoryList = [];
+      category.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.categoryList.push(x as Category);
+      });
+    });
   }
 
   onSubmit() {
-
+    console.log(this.categoryForm.value);
+    this.categoryService.insertCategories(this.categoryForm.value);
+    console.log("Correcto");
   }
+
+  editCategory(category: Category) {
+    console.log(category);
+  }
+
+  deleteCategory($key: string) {
+    this.categoryService.deleteCategories($key);
+  }
+
+
 }
