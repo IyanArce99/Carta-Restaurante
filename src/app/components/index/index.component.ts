1ec33854959183079ae1140b68419e3e839cb59f
iyan <iyan.arcega@neozink.com>
2020-12-20 20:13:08 +0100

    Promotions

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadService } from 'src/app/services/upload.service';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
   logo = '../../assets/logo.png';
-  constructor() { }
+  urls:any[];
+
+  constructor(private imgSvc: UploadService) { }
 
   ngOnInit(): void {
+    this.imgSvc.getImg().snapshotChanges().subscribe( data => {
+      this.urls = [];
+      data.forEach( element => {
+        let x = element.payload.toJSON();
+        this.urls.push(x);
+      })
+      this.getLastUrl();
+    })
+  }
+
+  getLastUrl() {
+    // this.url = this.urls[this.urls.length - 1];
+    console.log(this.urls[this.urls.length - 1]);
   }
 
 }
